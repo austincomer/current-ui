b704873213c4e8c02de502ee2168f6293f18c908
austincomer <austinmcomer@gmail.com>
2023-09-04 17:51:52 -0400

    feat: home page redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/docs/overview/introduction',
+        permanent: true,
+      },
+    ]
+  },
   experimental: {
     mdxRs: true,
   },
 }
- 
+
 const withMDX = require('@next/mdx')({
   // ...
   options: {
